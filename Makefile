dba861119faf690eb79ef532f01424fb58d6e64f
Heneros <punk60826@gmail.com>
2024-05-14 16:44:54 +0300

    docker

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 build:
-	docker compose -f local.yml up -d --remove-orphans
+	docker compose -f local.yml up -d --build --remove-orphans
 
 up:
-	docker compose -f local.yml up -d 
+	docker compose -f local.yml up -d  --watch
 
 show-logs-backend:
 	docker compose -f local.yml logs backend
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-console.log('user')
+
+import path from "path";
+
+import 'dotenv/config';
+import express from "express";
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World</h1>")
+});
+console.log(`123`)
+
+const port = process.env.PORT || 4000;
+
+const start = async () => {
+    try {
+        app.listen(port, console.log(`Working on ${port} port`))
+    } catch (error) {
+        console.log(error + 'error');
+    }
+}
+start()
