cfdce5fce6e8227b5fa8d1f222a5f46ded66cf69
Heneros <punk60826@gmail.com>
2024-05-21 20:59:30 +0300

    ...

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,86 @@ const MONGO_URI = process.env.MONGO_URI;
 // JWT_ACCESS_SECRET_KEY=4f5b4a0c2db99e7f304ab1238f28234759836a359d5708d10c8095fd9030
 // JWT_REFRESH_SECRET_KEY=bd1ba2c8956bbc89322d639b0a123173c7ef9e14c1e9acc29c2666cdfc57
 
+-------------------
+Code брать.
+mern invoice
+react project ulbi typescript
+-------------------
+Technology:
+.eslint связать и настроить
+docker compose 
+typescript 
+passport register google 
+сontact us
+cloudflare изображения все
+jest тест блоков
+webpack react project ulbi typescript для react запускалось приложение
+  Feature-Sliced Design (FSD).
+-------------------
+Pages:
+register
+login
+homepage
+verification page
+user profile 
+contact us
+shop
+reset password
+users list - admin page
+products list - admin page
+edit product - admin page
+single product add  Schedule a visit
+user profile можно download pdf 
+-------------------
+Дата сдача
+28 мая
+-------------------
+today
+concurrently добавить 
+mongodb ссылку
+
+
+
+-------------------
+tomorrow
+sendEmail сделать  при register
+создать пользователя
+логин пользователя 
+Logger
+
+-------------------
+remember
+npm cache clean --force
+npm init @eslint/config
+rm -rf node_modules
+
+
+
+Проксирование - это механизм, который позволяет передавать запросы от клиента к одному серверу (называемому прокси-сервером), который затем передает запросы другому серверу, выполняет необходимую обработку и возвращает результат клиенту.
+
+
+
+Заголовок Host - это часть HTTP-запроса, в которой клиент указывает имя хоста, к которому он хочет обратиться. Это необходимо, поскольку серверы могут хостить несколько сайтов на одном IP-адресе, и заголовок Host позволяет серверу определить, какой сайт он должен обслужить.
+
+
+  location / {
+    proxy_pass http://client;
+
+    proxy_redirect off;
+
+    proxy_set_header Host $host;
+
+
+Эта строка добавляет заголовок X-Real-IP в исходящий запрос. В него записывается значение $remote_addr, которое представляет собой IP-адрес клиента, сделавшего запрос. Это может быть полезно для сервера http://client для идентификации реального клиента, особенно если трафик проходит через несколько прокси-серверов.
+    proxy_set_header X-Real-IP $remote_addr;
+
+Эта строка добавляет заголовок X-Forwarded-For в исходящий запрос. В него записывается значение $proxy_add_x_forwarded_for, которое накапливает информацию обо всех IP-адресах прокси-серверов, через которые прошел запрос. Это может быть полезно для отслеживания пути запроса
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+    proxy_set_header X-Forwarded-Host $server_name;
+
+  }
+
 const start = async () => {
     try {
         app.listen(port, console.log(`Working on ${port} port`));
