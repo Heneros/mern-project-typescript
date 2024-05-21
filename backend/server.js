4e9badcfd5976515faa1553754de2589daa940b3
Heneros <punk60826@gmail.com>
2024-05-21 20:16:27 +0300

    env vars

--- backend/server.js
+++ backend/server.js
@@ -29,9 +29,20 @@ console.log('hello world123');
 
 const port = process.env.PORT || 4000;
 // local
-// const MONGO_URI = mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@mongodb/mernvilla
-
+const MONGO_URITest = `mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@mongodb/mernvilla`;
 const MONGO_URI = process.env.MONGO_URI;
+
+// PORT=1997
+
+// MONGO_ROOT_USERNAME=3bb8894935d74893907b0e552f50cb9597c6f410fab9b099b69ce3acea89
+// MONGO_ROOT_PASSWORD=ce0f697c7ab3a331a185
+
+// MONGO_URI=mongodb+srv://punk60826:5RDsLg75YdWpnV6X@cluster0.7jjv1y5.mongodb.net/mernVilla?retryWrites=true&w=majority
+
+// DOMAIN=http://localhost:8080
+
+// JWT_ACCESS_SECRET_KEY=4f5b4a0c2db99e7f304ab1238f28234759836a359d5708d10c8095fd9030
+// JWT_REFRESH_SECRET_KEY=bd1ba2c8956bbc89322d639b0a123173c7ef9e14c1e9acc29c2666cdfc57
 
 const start = async () => {
     try {
