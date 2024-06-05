1a0a3ba1698e80e256485ee3ce26ceb79f20873a
Heneros <punk60826@gmail.com>
2024-06-05 16:51:41 +0300

    ....

--- frontend/src/features/auth/authSlice.ts
+++ frontend/src/features/auth/authSlice.ts
@@ -1,10 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { decodeToken } from "react-jwt";
+import { User } from 'shared/types/User';
 
 
-
-const user = JSON.parse(localStorage.getItem("user"));
+// userString должен быть строкой, так как localStorage.getItem возвращает строку или null. Если localStorage не содержит значения для "user", вы предоставляете пустой массив [] в качестве значения по умолчанию. Это приведет к ошибке, потому что массив не может быть назначен строковой переменной.
+const userString = localStorage.getItem("user") || '';
 const googleToken = localStorage.getItem("googleToken");
+let user: User | null = null;
 
+if (userString) {
+  try {
+    user = JSON.parse(userString);
+  } catch (error) {
+    console.error("Error parsing user data:", error);
+  }
+}
 
-const decodedToken = decodeToken(googleToken);
+const decodedToken: User | null = googleToken ? decodeToken(googleToken) : null;
+
+interface AuthSlice {
+  user: User | null;
+  googleToken: string | null;
+}
+
+const initialState: AuthSlice = {
+  user: user || (decodedToken && typeof decodedToken === "object" ? decodedToken : null),
+  googleToken: googleToken ? googleToken : null,
+};
+
+export default initialState;
+
---
+++ frontend/src/shared/types/User.ts
@@ -0,0 +1,14 @@
+export interface User{
+    email: String,
+    username: String,
+    firstName: string;
+    lastName: string;
+    isEmailVerified: boolean;
+    googleID?: string;
+    avatar?: string;
+    businessName?: string;
+    phoneNumber?: string;
+    roles?: string[];
+    active?: boolean;
+    refreshToken?: string[];
+}
