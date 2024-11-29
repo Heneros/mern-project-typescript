c643a4a557d98c265b0d8da3feeb89d9624edbd9
Heneros <punk60826@gmail.com>
2024-11-29 21:00:20 +0200

    ...

--- backend/controllers/order/addOrderItem.ts
+++ backend/controllers/order/addOrderItem.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from 'express';
 import Property from '@/models/propertiesModel';
+import { Model } from 'mongoose';
+
+interface OrderType {
+    [_id: string]: string;
+}
 
 const addOrderItem = async (req: Request, res: Response) => {
     const { orderItems, paymentMethod } = req.body;
@@ -7,6 +12,22 @@ const addOrderItem = async (req: Request, res: Response) => {
         res.status(400).json({ message: 'No order items' });
     }
     const itemsFromDB = await Property.find({
-        _id: { $in: orderItems.map((x: string) => x._id) },
+        _id: { $in: orderItems.map((x: OrderType) => x._id) },
     });
+
+    const dbOrderItems = orderItems.map(
+        (itemFromClient: Model<OrderDocument>) => {
+            const matchingItemFromDB = itemsFromDB.find(
+                (itemFromDB) =>
+                    itemFromDB._id.toString() === itemFromClient._id,
+            );
+            return {
+                ...itemFromClient,
+                property: itemFromClient._id,
+                price: matchingItemFromDB.price,
+                _id: undefined,
+            };
+        });
+
+        const {itemsPrice, taxPrice, totalPrice} = 
 };
