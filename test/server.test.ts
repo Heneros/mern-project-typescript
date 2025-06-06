663507424eb7f8d234d9965a4dfb09a5d5c62fb7
Heneros <punk60826@gmail.com>
2025-06-06 18:58:05 +0300

    ...

--- test/server.test.ts
+++ test/server.test.ts
@@ -6,9 +6,28 @@ import { connectTestDB, disconnectTestDB } from './setupTestDB';
 
 export const request = supertest(app);
 
-
 beforeAll(async () => {
     await connectTestDB();
+
+    jest.mock('passport', () => ({
+        use: jest.fn(),
+        initialize: jest.fn(() => (req: any, res: any, next: any) => next()),
+        session: jest.fn(() => (req: any, res: any, next: any) => next()),
+        authenticate: jest.fn(() => (req: any, res: any, next: any) => next()),
+        serializeUser: jest.fn((fn) => fn(null, { id: 'test-user-id' })),
+        deserializeUser: jest.fn((fn) => fn(null, { id: 'test-user-id' })),
+    }));
+
+    jest.mock('passport-google-oauth20', () => {
+        return {
+            Strategy: jest.fn().mockImplementation(() => {
+                return {
+                    name: 'google',
+                    authenticate: jest.fn((req, options) => {}),
+                };
+            }),
+        };
+    });
 });
 
 afterAll(async () => {
