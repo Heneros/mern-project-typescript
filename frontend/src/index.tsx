662ec7bb40b4a78e0db6b82ba6265633deb5ce9a
Heneros <punk60826@gmail.com>
2025-07-06 14:31:12 +0300

    test webpack

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,8 +4,8 @@ import ReactDOM, { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from 'app/App';
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
+// import { loadStripe } from '@stripe/stripe-js';
+// import { Elements } from '@stripe/react-stripe-js';
 
 const container = document.querySelector('#root') as HTMLElement;
 const root = createRoot(container);
